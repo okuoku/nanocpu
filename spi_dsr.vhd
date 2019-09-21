6f18b2179c364dad864d3e64b7f5a05c00b5ab62
okuoku <mjt@cltn.org>
2019-09-21 15:22:05 +0900

    spi device: Correct SPI capture phase

--- spi_dsr.vhd
+++ spi_dsr.vhd
@@ -22,6 +22,7 @@ architecture arch_spi_dsr of spi_dsr is
     signal reg: std_logic_vector(7 downto 0);
     type type_state is (s0,s1,s2,s3,s4,s5,s6,s7);
     signal state: type_state;
+    signal buf: std_logic;
     attribute fsm_encoding: string;
     attribute fsm_encoding of state: signal is "compact";
 begin
@@ -30,28 +31,30 @@ begin
         if ss_n = '1' then
             state <= s0;
             reg <= data_in;
+        elsif rising_edge(sclk) then
+            buf <= si;
         elsif falling_edge(sclk) then
             if state = s0 then
                 state <= s1;
-                reg <= reg(6 downto 0) & si;
+                reg <= reg(6 downto 0) & buf;
             elsif state = s1 then
                 state <= s2;
-                reg <= reg(6 downto 0) & si;
+                reg <= reg(6 downto 0) & buf;
             elsif state = s2 then
                 state <= s3;
-                reg <= reg(6 downto 0) & si;
+                reg <= reg(6 downto 0) & buf;
             elsif state = s3 then
                 state <= s4;
-                reg <= reg(6 downto 0) & si;
+                reg <= reg(6 downto 0) & buf;
             elsif state = s4 then
                 state <= s5;
-                reg <= reg(6 downto 0) & si;
+                reg <= reg(6 downto 0) & buf;
             elsif state = s5 then
                 state <= s6;
-                reg <= reg(6 downto 0) & si;
+                reg <= reg(6 downto 0) & buf;
             elsif state = s6 then
                 state <= s7;
-                reg <= reg(6 downto 0) & si;
+                reg <= reg(6 downto 0) & buf;
             elsif state = s7 then
                 state <= s0;
                 reg <= data_in;
@@ -62,6 +65,6 @@ begin
     so <= reg(7);
     data_in_ack <= '1' when state = s0 else '0';
     data_out_rdy <= '1' when state = s7 and sclk = '1' else '0';
-    data_out <= reg(6 downto 0) & si;
+    data_out <= reg(6 downto 0) & buf;
 
 end arch_spi_dsr;
